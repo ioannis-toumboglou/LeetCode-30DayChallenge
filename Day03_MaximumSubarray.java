2ce63d5fcaa8d441decd394177f10b0ca79707b3
Ioannis Toumboglou <ioannis.toumboglou@gmail.com>
2020-04-03 23:00:11 +0300

    Add files via upload

---
+++ Day03_MaximumSubarray.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class Day03_MaximumSubarray {
+
+	public static int maxSubArray(int[] array) {
+		int size = array.length;
+		int max_so_far = array[0]; 
+	    int curr_max = array[0]; 
+	  
+	    for (int i = 1; i < size; i++) 
+	    { 
+	           curr_max = Math.max(array[i], curr_max+array[i]); 
+	        max_so_far = Math.max(max_so_far, curr_max); 
+	    } 
+	    return max_so_far; 
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int[] array = {1,2,-1,-2,2,1,-2,1};
+		
+		System.out.println(maxSubArray(array));
+	}
+
+}
