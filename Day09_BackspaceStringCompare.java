38939019394fc1e2224e42d0f7d8eae8d41594c9
Ioannis Toumboglou <ioannis.toumboglou@gmail.com>
2020-04-10 00:40:54 +0300

    Add files via upload

---
+++ Day09_BackspaceStringCompare.java
@@ -0,0 +1,45 @@
+package LeetCode;
+
+public class Day09_BackspaceStringCompare {
+	
+	public static String removeHashes(String s) {
+		
+		StringBuilder sSB = new StringBuilder(s);
+		
+		while (sSB.toString().contains("#")) {
+			
+			int index = sSB.indexOf("#");
+			
+			if (index == 0) {
+				sSB.deleteCharAt(index);
+			} else {
+				sSB.deleteCharAt(index);
+				sSB.deleteCharAt(--index);
+			}
+		}
+
+		return sSB.toString();
+	}
+	
+	public static boolean backspaceCompare(String s, String t) {
+		
+		s = removeHashes(s);
+		t = removeHashes(t);
+		
+		if (s.equals(t)) {
+			return true;
+		}
+		
+		return false;
+	}
+
+	public static void main(String[] args) {
+		
+		String s = "a##c";
+		String t = "#a#c";
+		
+		System.out.println(backspaceCompare(s, t));
+		
+	}
+
+}
